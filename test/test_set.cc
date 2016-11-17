bfd644b9258f25b1038bd9506a942ef94e33e1e2
Richard Frank <richafrank@gmail.com>
2016-11-17 09:22:40 -0500

    more set testing

--- test/test_set.cc
+++ test/test_set.cc
@@ -18,11 +18,30 @@ TEST(Set, type) {
 }
 
 TEST(Set, iteration) {
-    // std::array<py::object, 3> expected({0_p, 1_p, 2_p});
+    auto expected = py::list::pack(0_p, 1_p, 2_p);
+    py::list::object actual(0);
     auto ob = py::set::pack(0_p, 1_p, 2_p);
     std::size_t n = 0;
 
     ASSERT_EQ(ob.len(), 3);
+    for (const auto &e : ob) {
+        ASSERT_TRUE((std::is_same<decltype(e), const py::object&>::value)) <<
+            "const iteration over ob does not yield correct type";
+        actual.append(e);
+        n++;
+    }
+    ASSERT_EQ(n, 3u) << "ran through too many iterations";
+    EXPECT_EQ(PyList_Sort(actual), 0);
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(Set, uniqueness) {
+    auto expected = py::set::pack(0_p, 1_p, 2_p);
+    auto ob = py::set::pack(0_p, 1_p, 2_p, 0_p, 1_p);
+    std::size_t n = 0;
+
+    ASSERT_EQ(ob.len(), 3);
+    ASSERT_EQ(ob, expected);
     for (const auto &e : ob) {
         ASSERT_TRUE((std::is_same<decltype(e), const py::object&>::value)) <<
             "const iteration over ob does not yield correct type";
